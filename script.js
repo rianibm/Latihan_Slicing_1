54c591c4b9b90fecd13b90864b782009c1074f5d
Riani BM <rianimatialu@gmail.com>
2023-09-09 15:34:11 +0700

    update navbar

--- script.js
+++ script.js
@@ -4,15 +4,31 @@ $(document).ready(function () {
   // Function to update the header styles
   function updateHeaderStyles() {
     if ($(window).scrollTop() > 0) {
-      header.addClass("scroll shadow-sm"); // Add the 'scroll' and 'shadow-sm' classes
-      // Change font color of navigation links to black
+      header.addClass("scroll shadow-sm");
       $(".nav-link").removeClass("text-white").addClass("text-black");
     } else {
-      header.removeClass("scroll shadow-sm"); // Remove the 'scroll' and 'shadow-sm' classes
-      // Change font color of navigation links to white
+      header.removeClass("scroll shadow-sm");
       $(".nav-link").removeClass("text-black").addClass("text-white");
     }
   }
+
+  // Function to handle smooth scrolling when a navigation link is clicked
+  $(".nav-link").on("click", function (e) {
+    e.preventDefault(); // Prevent the default behavior of the link
+    var targetId = $(this).attr("href"); // Get the target section's ID from the link's href attribute
+    var targetElement = $(targetId); // Find the target element using the ID
+
+    if (targetElement.length) {
+      // Check if the target element exists on the page
+      var offset = targetElement.offset().top - header.outerHeight(); // Calculate the scroll offset
+      $("html, body").animate(
+        {
+          scrollTop: offset,
+        },
+        1000 // Adjust the animation duration as needed
+      );
+    }
+  });
 
   // Initial call to set styles on page load
   updateHeaderStyles();
